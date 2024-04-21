d9fbc5477f9ea7df64e13673d0bf7a5fc8ec4c81
MatiasG19 <m.galusek@gmail.com>
2024-04-21 13:35:38 +0200

    Fix routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.galusek.de',
-  base: '/',
+  base: '/galusek-k-and-k',
   integrations: [tailwind()]
 });
