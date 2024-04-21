87c8b12955e010c14b6ffe23854e8d4a951918bf
MatiasG19 <m.galusek@gmail.com>
2024-04-21 15:49:56 +0200

    Fix path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.galusek.de',
-  base: '/galusek-k-and-k',
+  base: '/',
   integrations: [tailwind()]
 });
